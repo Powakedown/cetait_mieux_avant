1a0100860f30fecfc4dd95cb7f9c15d848ec5358
Maxime Boué <null>
2025-05-23 11:00:49 +0200

    Add historical assets to seeds with descriptions and sources

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,110 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# Meilleure avancées technologiques humaines
+# - eau potable au robinet
+# - électricité domestique
+# - internet
+# - antibiotiques
+# - lèpre
+# - voiture
+# - salle de bain à domicile
+# - démocratie
+# - travail des enfants
+# - vote des femmes
+# - esclavage
+
+Asset.create(
+  name: "Eau potable",
+  comment: "Eau potable au robinet en France disponible couramment dans les foyers, environ 80% des foyers en 1950.",
+  source: "https://fr.wikipedia.org/wiki/Eau_potable_en_France",
+  start_year: "1950",
+  end_year: "2025",
+  order: 1
+)
+Asset.create(
+  name: "Électricité domestique",
+  comment: "Électricité disponible dans la majorité de foyers français après la guerre, dans les années 1950.",
+  source: "https://fr.wikipedia.org/wiki/%C3%89lectricit%C3%A9_en_France",
+  start_year: "1950",
+  end_year: "2025",
+  order: 2
+)
+Asset.create(
+  name: "Internet",
+  comment: "Utilisation grand publique d'Internet en France, environ 50% des foyers en 2000.",
+  source: "https://fr.wikipedia.org/wiki/Internet_en_France",
+  start_year: "2000",
+  end_year: "2025",
+  order: 3
+)
+Asset.create(
+  name: "Antibiotiques",
+  comment: "Découverte de la pénicilline par Alexander Fleming en 1928, mais utilisation généralisée dans les années 1940.",
+  source: "https://fr.wikipedia.org/wiki/P%C3%A9nicilline",
+  start_year: "1940",
+  end_year: "2025",
+  order: 2
+)
+Asset.create(
+  name: "Lèpre",
+  comment: "Présence de la lèpre en France, éradication de la maladie dans les années 1950.",
+  source: "https://fr.wikipedia.org/wiki/L%C3%A8pre",
+  start_year: "1950",
+  end_year: "2025",
+  order: 2
+)
+Asset.create(
+  name: "Voiture",
+  comment: "Possession de la voiture dans les foyers français, environ 50% des foyers en 1968.",
+  source: "https://fr.wikipedia.org/wiki/Voiture",
+  start_year: "1968",
+  end_year: "2025",
+  order: 3
+)
+
+Asset.create(
+  name: "Salle de bain",
+  comment: "En 1970, environ 50 % des maisons françaises étaient équipées de salles de bain",
+  source: "https://fr.wikipedia.org/wiki/Salle_de_bain",
+  start_year: "1970",
+  end_year: "2025",
+  order: 4
+)
+
+Asset.create(
+  name: "Démocratie",
+  comment: "La démocratie en France a été instaurée en 1789 avec la Révolution française.",
+  source: "https://fr.wikipedia.org/wiki/D%C3%A9mocratie_en_France",
+  start_year: "1789",
+  end_year: "2025",
+  order: 5
+)
+
+Asset.create(
+  name: "Travail des enfants",
+  comment: "La loi de 1904 a interdit le travail des enfants de moins de 13 ans en France.",
+  source: "https://fr.wikipedia.org/wiki/Travail_des_enfants",
+  start_year: "1904",
+  end_year: "2025",
+  order: 6
+)
+
+Asset.create(
+  name: "Vote des femmes",
+  comment: "Le droit de vote des femmes a été accordé en France en 1944.",
+  source: "https://fr.wikipedia.org/wiki/Droit_de_vote_des_femmes_en_France",
+  start_year: "1944",
+  end_year: "2025",
+  order: 7
+)
+
+Asset.create(
+  name: "Esclavage",
+  comment: "L'esclavage a été aboli en France en 1848.",
+  source: "https://fr.wikipedia.org/wiki/Esclavage_en_France",
+  start_year: "1848",
+  end_year: "2025",
+  order: 8
+)
+
+
+
+
